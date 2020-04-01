ef6d2fd153148a1b430fb98e85eab512e91429c7
19961990 <62289218+19961990@users.noreply.github.com>
2020-04-01 12:56:14 +0200

    Update R_code_point_patterns.r

--- R_code_point_patterns.r
+++ R_code_point_patterns.r
@@ -15,7 +15,6 @@ plot(covid$country,covid$cases,las=1) # horizontal labels.las mette in verticale
 plot(covid$country,covid$cases,las=3)#perpandiculars labels
 plot(covid$country,covid$cases,las=3,cex.lab=0.5,cex.axis=0.5) # vertical labels.cex.lab o cex.axis permette di ingrandire diminuire le dimensione del graph
 #ggplot2
-
 data(mpg)
 head(mpg)
 
@@ -30,29 +29,44 @@ ggplot(covid,aes(x=lon,y=lat,size=cases)) + geom_point()#grandezza dei punti o p
 #density
 #create dataset for spatstat
 attach(covid)
-covids<-ppp(lon,lat,c(-180,180),c(-90,90))
-
-library(spatstat)
-library(rgdal)
+covidsppp<-(lon,lat,c(-180,180),c(-90,90))
+ 
+d<- density(covids)
+plot(d)
+points(covids)
 
 > ppp(x.coordinates,y.coordinates,x.range,y.range)
-load("point_pattern.RData") # inerire l'RData nominato ieri
-
-# inserire le coastlines sui dati iol nella cartella lab
-coastlines <- readOGR("ne_10m_coastline.shp")
 
 plot(d)
 points(covids,pch=19,cex=0.5) #pch point character
-plot(coastlines, col="yellow", add=T)
 
-# Exercise: plot the density of the point pattern with a differnt color palette
+#SAVE the Rdata
+setwd("C:/lab")
+load("point_pattern.RData") # inerire l'RData nominato ieri
+ls()
+library(spatstat)
+plot(d)
 
-cl <- colorRampPalette(c('yellow','orange','red'))(100) # 
-plot(d,col=cl)
-points(covids,pch=19,cex=0.5)
+#palette per chiamare i colori cl
+cl <- colorRampPalette(c('yellow','orange','red'))(100) # 100 quanto livelli di colore vogliamo,100 gradazione di colore
+plot(d,col=cl) # CL l ogetto
+
+# Exercise: plot the density of the point pattern with a differnt color palette.plot della mappa della densita dal verde a blu
+cl <- colorRampPalette(c('green','blue'))
+plot(d,col=cl)#per plottare sul grafico
+points(covids)
+# inserire le coastlines sui dati iol nella cartella lab paesi della costa
+coastlines <- readOGR("ne_10m_coastline.shp") #read quello dentro la cartella labinstall.packages("rgdal")
+install.packages("rgdal")
+library(rgdal) #per leggere file vettoriale
+
+coastlines <- readOGR("ne_10m_coastline.shp")
+plot(coastlines, add=T)
+plot(coastlines, col="yellow", add=T) #add aggiungere le coastlines
 plot(coastlines, col="blue", add=T)
-
-
+#excercise plot della mappa di densita con una nuova colorazione  e aggiunta delle coastlines
+cl <- colorRampPalette(c('red','orange','yellow','green', 'blue'))(100)
+plot(coastlines, col="yellow", add=T)
 
 
 
